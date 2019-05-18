fd1a80f5960566b607e0333ae9ba43e253ab891b
Connor Wilhoit <connorwilhoit1@gmail.com>
2019-05-18 00:05:05 -0500

    Delete preprocess_image_from_path.py

--- preprocess_image_from_path.py
+++
@@ -1,7 +0,0 @@
-def preprocess_image_from_path(image_path, speed, brightness_factor):
-    img = cv2.imread(image_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = change_brightness(img, brightness_factor)
-    img = preprocess_image(img)
-    return img, speed
-
