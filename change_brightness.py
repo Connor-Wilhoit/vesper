52fcac1aa0c2e15aee21d5404bcdba26b713cb99
Connor Wilhoit <connorwilhoit1@gmail.com>
2019-05-18 00:04:16 -0500

    Delete change_brightness.py

--- change_brightness.py
+++
@@ -1,5 +0,0 @@
-def change_brightness(image, brightness_factor):
-    hsv_image          = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    hsv_image[:, :, 2] = hsv_image[:, :, 2] * brightness_factor
-    image_rgb          = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2RGB)
-    return image_rgb
