1bf10aa4a37e2fed1ef5ca1e1c7a6ed0d9716f0f
Connor Wilhoit <connorwilhoit1@gmail.com>
2019-05-18 00:05:16 -0500

    Delete preprocess_image_valid_from_path.py

--- preprocess_image_valid_from_path.py
+++
@@ -1,6 +0,0 @@
-def preprocess_image_valid_from_path(image_path, speed):
-    img = cv2.imread(image_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = preprocess_image(img)
-    return img, speed
-
